9f4dc0c62d492bf064da80f75c50c25efc15a97e
Robby Callicotte <rcallicotte@fedoraproject.org>
2025-11-09 16:42:03 -0600

    Add tests for konsolerc
    Added notes for elisa config research.

--- test/integration/kde/roles/desktop/test_local.py
+++ test/integration/kde/roles/desktop/test_local.py
@@ -1,3 +1,54 @@
 # test_local.py
 # Run tests for desktop role
 
+
+from docitlib import read_yaml
+
+
+
+PLAYPATH = "roles/desktop"
+USER = read_yaml("vars/desktop.yml")['user']
+
+def test_deploy_konsolerc_config__file_exists(host):
+    """Verify file exists"""
+    assert host.file(f"/home/{USER['username']}/.config/konsolerc").exists
+
+
+def test_deploy_konsolerc_config__file_owner(host):
+    """Verify file owner"""
+    assert host.file(f"/home/{USER['username']}/.config/konsolerc").user == USER['username']
+
+
+def test_deploy_konsolerc_config__file_group(host):
+    """Verify file group"""
+    assert host.file(f"/home/{USER['username']}/.config/konsolerc").group == USER['username']
+
+
+def test_create_konsole_config_dir__file_dir(host):
+    """Verify directory exists"""
+    assert host.file(f"/home/{USER['username']}/.local/share/konsole").is_directory
+
+
+def test_create_konsole_config_dir__file_owner(host):
+    """Verify directory owner"""
+    assert host.file(f"/home/{USER['username']}/.local/share/konsole").user == USER['username']
+
+
+def test_create_konsole_config_dir__file_group(host):
+    """Verify directory group"""
+    assert host.file(f"/home/{USER['username']}/.local/share/konsole").group == USER['username']
+
+
+def test_deploy_konsole_profile__file_exists(host):
+    """Verify file exists"""
+    assert host.file(f"/home/{USER['username']}/.local/share/konsole/green.profile").exists
+
+
+def test_deploy_konsole_profile__file_owner(host):
+    """Verify file owner"""
+    assert host.file(f"/home/{USER['username']}/.local/share/konsole/green.profile").user == USER['username']
+
+
+def test_deploy_konsole_profile__file_group(host):
+    """Verify file group"""
+    assert host.file(f"/home/{USER['username']}/.local/share/konsole/green.profile").group == USER['username']
